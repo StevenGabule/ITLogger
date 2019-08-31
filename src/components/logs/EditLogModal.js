4ec1f4a8b25ef51c65e6d1265019229107fc10a0
John Paul Lim Gabule <lucas.gabule@gmail.com>
2019-09-01 05:37:29 +0800

    implement redux and components and ui in react

---
+++ src/components/logs/EditLogModal.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import M from 'materialize-css/dist/js/materialize.min';
+import {updateLog} from "../../actions/LogAction";
+
+const EditLogModal = ({updateLog, current}) => {
+    const [message, setMessage] = useState('');
+    const [tech, setTech] = useState('');
+    const [attention, setAttention] = useState(false);
+
+    useEffect(() => {
+       if (current) {
+           setMessage(current.message);
+           setAttention(current.attention);
+           setTech(current.tech);
+       }
+    }, [current]);
+
+    const onSubmit = () => {
+        if (message === '' || tech === '') {
+            M.toast({html: 'Please enter a message or tech'})
+        } else {
+            const updLog = {
+               id: current.id,
+               message,
+               attention,
+               tech,
+               date: new Date()
+            };
+
+            updateLog(updLog);
+            M.toast({html: `Log updated by ${tech}`});
+
+            setMessage('');
+            setTech('');
+            setAttention(false);
+        }
+
+    };
+    return (
+        <div id='edit-log-modal' className="modal" style={modalStyle}>
+            <div className="modal-content">
+                <h4>Enter System Log</h4>
+                <div className="row">
+                    <div className="input-field">
+                        <input type="text" name='message' value={message} onChange={e => setMessage(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <select name="tech" value={tech} className='browser-default'
+                                onChange={e => setTech(e.target.value)}>
+                            <option value="" disabled>Select Technician</option>
+                            <option value="john doe">John Doe</option>
+                            <option value="sam smith">Sam Smith</option>
+                            <option value="sara wilson">Sara Wilson</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <p>
+                            <label>
+                                <input type="checkbox" className='filled-in' checked={attention} value={attention}
+                                       onChange={e => setAttention(!attention)}/>
+                                <span>Needs Attention</span>
+                            </label>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a href="#!" onClick={onSubmit} className='modal-close waves-effect blue btn'>Enter</a>
+            </div>
+        </div>
+    );
+};
+
+const modalStyle = {
+    width: '75%',
+    height: '75%'
+};
+EditLogModal.propTypes = {
+    current: PropTypes.object.isRequired,
+    updateLog: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+   current: state.log.current
+});
+export default connect(mapStateToProps, {updateLog})(EditLogModal);
